71bb99dc26b0b51669ab0bfe4bc4143fda000cfd
MatiuninaMarina <aprelkina.marina@mail.ru>
2023-02-17 14:43:08 +0400

    Add min() and average() methods in SalesManager class

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,11 @@ public class Main {
         SalesManager salesManager = new SalesManager(sales);
 
         int max = salesManager.max();
+        int min = salesManager.min();
+        int average = salesManager.average();
 
         System.out.println(max);
-
+        System.out.println(min);
+        System.out.println(average);
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,5 +14,23 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int min() {
+        int min = Integer.MAX_VALUE;
+        for (int sale : sales) {
+            if (min > sale) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int average() {
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+        }
+    return (sum - (min() + max()))/ (sales.length - 2);
+}
 }
 
